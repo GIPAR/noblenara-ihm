8d8b158a969b67bb90e9e28d1b4f74462584dba4
luizfiliperibeiro <lipehribeiro2018@gmail.com>
2026-05-28 18:10:05 -0300

    Atualizado menu lateral com botão para VoiceChat

--- nara-ihm/src/App.tsx
+++ nara-ihm/src/App.tsx
@@ -10,6 +10,7 @@ import { useStore } from 'zustand'
 import { GlobalStore, ROStore } from './contexts/Store'
 import { useSetAtom, useAtom } from 'jotai';
 import { LocationAtom, SpeedAtom, RobotAtom, ThemeAtom } from './contexts/Molecule';
+import { VoiceChatAtom } from "./contexts/Molecule";
 
 function App() {
   const connectionAttemptedRef = useRef(false);
@@ -20,6 +21,7 @@ function App() {
   const setMaxSpeed = useSetAtom(SpeedAtom)
   const [Theme] = useAtom(ThemeAtom)
   const [Robot] = useAtom(RobotAtom)
+  const [ShowVoiceChat] = useAtom(VoiceChatAtom)
 
   useEffect(() => {
     if (!connectionAttemptedRef.current) {
@@ -54,7 +56,7 @@ function App() {
     <div className={`App ${Theme}`}>
       <main>
         <MessageLog/>
-        <VoiceChat />
+        {ShowVoiceChat ? <VoiceChat /> : null}
 
         <div className='App-header'> <h1>
           {Robot.robot === 0 ? ( "NARA - Robot HMI"  ):( null )}
